7c7b728e6d5f66d37f41df6069db268d1cf8db4c
THE GOAT <aboabbasabo2017@gmail.com>
2023-02-17 04:56:06 +0200

    updated JS

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const calendar = document.getElementById('calendar');
+const monthEl = document.getElementById('month');
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
 
 const drawBlankCalendar = () => {
     for (let i = 0; i < 35; i++) {
@@ -7,7 +27,8 @@ const drawBlankCalendar = () => {
 
         const dayText = document.createElement('p');
         dayText.classList.add('day-text');
-        
+        dayText.innerText = days[i % 7];
+
         const dayNumber = document.createElement('p');
         dayNumber.classList.add('day-number');
         
@@ -21,4 +42,36 @@ const drawBlankCalendar = () => {
     }
 };
 
-drawBlankCalendar();
+const updateCalendar = (month, year, events) => {
+    const dayElements = document.querySelectorAll('.day');
+
+    const theFirst = new Date();
+    theFirst.setMonth(month);
+    theFirst.setYear(year);
+
+    const theFirstDayOfWeek = theFirst.getDay();
+    const monthName = months[month];
+    const monthWithYear = `${year} - ${monthName}`;
+    monthEl.innerText = monthWithYear;
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    let dayCounter = 1;
+    for (let i = 0; i < dayElements.length; i++) {
+        const day = dayElements[i];
+
+        const dayNumber = day.querySelector('.day-number');
+        if (i >= theFirstDayOfWeek  && dayCounter <= daysInMonth) {
+            dayNumber.innerText = dayCounter;
+            dayCounter++;
+        }
+        else {
+            dayNumber.innerText = '';
+        }
+
+    }
+};
+
+
+
+drawBlankCalendar()
+updateCalendar(currentMonth, currentYear)
